49590e55ced4096a29071f953da221b27bc00bd1
Tyler Hofer <tylerhofer@Tylers-MacBook-Pro.local>
2018-06-20 16:55:16 -0600

    did some styling on app.js

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default class App extends React.Component {
   render() {
     return (
+
       <View style={styles.container}>
-        <Text>App.js to start working on your app!</Text>
+        <Text style={styles.title}>Someones There</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+           title="Create Account"
+           color="#841584"
+           accessibilityLabel="Learn more about this purple button"
+        />
       </View>
     );
   }
@@ -16,8 +23,12 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#91e4fb',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  }
 });
