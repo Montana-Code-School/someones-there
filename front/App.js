6f0c9f1efb7a11c7b5c8602172de7a41a20f9093
Claire O'Brien <claireobrien@Claires-MacBook-Pro.local>
2018-06-19 20:34:07 -0600

    added the aws library and got the login working

--- front/App.js
+++ front/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
@@ -21,3 +25,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default withAuthenticator(App);
