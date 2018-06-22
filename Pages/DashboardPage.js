410ce730125e69156e9227ab9d01e25434ac6a4a
Claire O'Brien <claireobrien@Claires-MacBook-Pro.local>
2018-06-22 15:02:59 -0600

    further cleaned up code

--- Pages/DashboardPage.js
+++ Pages/DashboardPage.js
@@ -4,76 +4,81 @@ import { withNavigation } from 'react-navigation';
 import {Text, Button, Avatar } from 'react-native-elements';
 import ModalDropdown from 'react-native-modal-dropdown';
 
-  class DashboardPage extends React.Component {
-      static navigationOptions = {
-      title: "Dashboard",
-      };
-      render() {
-        return (
-     <ScrollView style={styles.container}>
-       <ModalDropdown options={['Settings', 'Log Out', 'Find Help']}/>
-        <View style={styles.titleContainer}>
-          <Avatar
-            size="medium"
-            rounded
-            source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"}}
-            onPress={() => console.log("Works!")}
-            activeOpacity={0.7}
-          />
-          <Text h2>Welcome, User!</Text>\
-        </View>
-        <View style={styles.textView}>
-           <Text h4>How is your day going today?</Text>
-        </View>
-        <View style={styles.imageView}>
-           <Image
-              style={styles.pic}
-              source={require('../placement-pictures/pug.jpg')}
-           />
-            <Text h3></Text>
-            <Text style={styles.taglineStyle}>insert quote here</Text>
-          </View>
-      </ScrollView>
-        );
-      }
-    }
+class DashboardPage extends React.Component {
 
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#91e4fb',
-      },
-      titleContainer: {
-        justifyContent: 'space-evenly',
-        flexDirection: "row",
-        margin: 15
-    },
-      pic: {
-        height: 270,
-        width: 270,
-        alignContent: "center",
-      },
-      textView:{
-        justifyContent: "center",
-        alignItems: "center",
-        marginBottom: 10,
-      },
-      imageView:{
-        justifyContent: "center",
-        alignItems: "center",
-        marginBottom: 10,
-      },
-      buttons: {
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 25
-      },
-      taglineStyle: {
-        fontSize: 15,
-        textAlign: 'center',
-        marginTop: 10
-      }
-    });
+static navigationOptions = {
+  title: "Dashboard",
+};
 
-    export default withNavigation(DashboardPage);
+render() {
+return (
+    <ScrollView style={styles.container}>
+      <ModalDropdown options={['Settings', 'Log Out', 'Find Help']}/>
+      <View style={styles.titleContainer}>
+        <Avatar
+          size="medium"
+          rounded
+          source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"}}
+          onPress={() => console.log("Works!")}
+          activeOpacity={0.7}
+        />
+        <Text h2>Welcome, User!</Text>\
+      </View>
+      <View style={styles.textView}>
+        <Text h4>How is your day going today?</Text>
+      </View>
+      <View style={styles.imageView}>
+        <Image
+           style={styles.pic}
+           source={require('../placement-pictures/pug.jpg')}
+        />
+        <Text h3></Text>
+        <Text style={styles.taglineStyle}>insert quote here</Text>
+      </View>
+    </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  backgroundColor: '#91e4fb',
+},
+titleContainer: {
+  justifyContent: 'space-evenly',
+  flexDirection: "row",
+  margin: 15
+},
+pic: {
+  height: 270,
+  width: 270,
+  alignContent: "center",
+},
+textView:{
+  justifyContent: "center",
+  alignItems: "center",
+  marginBottom: 10,
+},
+imageView:{
+  justifyContent: "center",
+  alignItems: "center",
+  marginBottom: 10,
+},
+buttons: {
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  marginTop: 25
+},
+taglineStyle: {
+  fontSize: 15,
+  textAlign: 'center',
+  marginTop: 10
+}
+});
+
+//Wrapping the entire component in
+//the withNavigation function allows us to
+//access this.props.navigation.navigate
+export default withNavigation(DashboardPage);
